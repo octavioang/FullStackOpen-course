6d9af6d04870a65341339714dd507dc298860ed3
octavioang <109118510+octavioang@users.noreply.github.com>
2022-08-09 02:59:30 +0200

    Exercises 2.15.-2.20.
    Exercises 2.15.-2.20.

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
+import './index.css'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Person'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
+import personService from './services/Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone] = useState('')
   const [ newNameFilter, setNewNameFilter ] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+
+
+  const hook = () => {
+    personService
+    .getAll()
+    .then(initialNotes => {
+      setPersons(initialNotes)
+    })
+  }
+
+  useEffect(hook, [])
 
   
 
@@ -25,18 +36,57 @@ const App = () => {
     }
     
     if (found === undefined){
-      setPersons(persons.concat(phoneObject))
+      personService.create(phoneObject).then(newItem => {setPersons(persons.concat(newItem))})
+        setErrorMessage(
+          `Added ${newName}`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      
       setNewName('')
       setNewPhone('')
     } else{
-      window.alert(newName + " is already added to phonebook"); //" is already added to phonebook"
+      if (window.confirm(newName + " is already added to phonebook, replace the old number with a new one?")){ //" is already added to phonebook"
+        personService.update(found.id,phoneObject).then(returnedPerson => {
+          setPersons(persons.map(person => person.id !== found.id ? person : returnedPerson))
+          setErrorMessage(
+            `${newName} was update`
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+        }).catch(error => {
+            console.log('fail')
+            setErrorMessage(
+              `[ERROR] ${newName} was already deleted from server`
+            )
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+          })
+      } 
       setNewName('')
       setNewPhone('')
     }
 
   }
 
+  const deletePerson = id => {
 
+    const filteredPerson = persons.filter(person => person.id === id)
+    console.log(filteredPerson)
+    const personName = filteredPerson[0].name
+    const personId = filteredPerson[0].id
+    if (window.confirm(`Delete ${personName} ?`)) {
+      
+      personService.remove(personId).then(setPersons(persons.filter(person => person.id !== personId))
+     )}
+  }
+
+
+  
+  
   const handleNameChange = (event) => {
       console.log(event.target.value)
       setNewName(event.target.value)
@@ -51,16 +101,17 @@ const App = () => {
   }
 
   const personToShow = (!newNameFilter) ? persons: persons.filter(person => person.name.toLocaleLowerCase().includes(newNameFilter.toLocaleLowerCase()))
-  console.log(!newNameFilter)
+  
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter newNameFilter={newNameFilter} handleFilterChange={handleFilterChange}/>
       <h3>Add a new</h3>
       <PersonForm addPhone={addPhone} newName={newName} handleNameChange ={handleNameChange} newPhone={newPhone} handlePhoneChange={handlePhoneChange}/>
       <h3>Numbers</h3>
-      <Persons persons={personToShow}/>
+      <Persons persons={personToShow} deletePerson={deletePerson}/>
     </div>
   )
 }
--- Part2/phonebook/src/components/Person.js
+++ Part2/phonebook/src/components/Person.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Person = ({ persons }) => {
+const Person = ({ persons,deletePerson }) => {
   
   return (
   <div>       
     <ul>
-  {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+  {persons.map(person => <li key={person.name}>{person.name} {person.number}  <button onClick={() => deletePerson(person.id)}>delete</button></li>)}
     </ul>
 </div>
   
